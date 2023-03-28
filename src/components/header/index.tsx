11013497d5c7d50d558ad16e2b6b2fc2e693c7f5
Carlos Nardo <c.cesar.nardo@gmail.com>
2023-03-28 16:33:15 -0300

    chore: page config context deleted. some nextjs config improvements
    (cherry picked from commit 39850a610358e661a24a619db0262d1430e1e7a6)

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,11 +1,8 @@
 import { StepperComponent } from "components";
-import { usePageConfig } from "providers/contexts";
 import { FC } from "react";
 import { HeaderContainer, HeaderContent } from "./styles";
 
 export const HeaderComponent: FC = () => {
-  const { steps } = usePageConfig();
-
   return (
     <HeaderContainer>
       <HeaderContent>
