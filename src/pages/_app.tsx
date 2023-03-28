39850a610358e661a24a619db0262d1430e1e7a6
Carlos Nardo <c.cesar.nardo@gmail.com>
2023-03-28 16:33:15 -0300

    chore: page config context deleted. some nextjs config improvements

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,6 @@ import {
 } from "components";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import { PageConfigProvider } from "providers/contexts";
 import { ThemeProvider } from "styled-components";
 
 import "styles/globals.css";
@@ -14,25 +13,20 @@ import { customTheme } from "styles/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <PageConfigProvider>
-      <ThemeProvider theme={customTheme}>
-        <PageWrapper>
-          <Head>
-            <title>JD Power ZappyRide</title>
-            <meta
-              name="viewport"
-              content="width=device-width, initial-scale=1"
-            />
-            <link rel="icon" href="/favicon.ico" />
-          </Head>
-          <HeaderComponent />
-          <PageBackground>
-            <PageContent>
-              <Component {...pageProps} />
-            </PageContent>
-          </PageBackground>
-        </PageWrapper>
-      </ThemeProvider>
-    </PageConfigProvider>
+    <ThemeProvider theme={customTheme}>
+      <PageWrapper>
+        <Head>
+          <title>JD Power ZappyRide</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <HeaderComponent />
+        <PageBackground>
+          <PageContent>
+            <Component {...pageProps} />
+          </PageContent>
+        </PageBackground>
+      </PageWrapper>
+    </ThemeProvider>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,7 @@
-import type { PageConfigType } from "types";
-
 import { HomeComponent } from "features/home";
 
-interface HomeProps {
-  data: PageConfigType;
-}
-
-const Home = ({ data }: HomeProps) => {
-  console.log(data);
-
+const Home = () => {
   return <HomeComponent />;
 };
-
-export async function getServerSideProps() {
-  const res = await fetch(`${process.env.BASE_URL}/api/configuration`);
-  const data = await res.json();
-
-  return { props: { data } };
-}
 
 export default Home;
